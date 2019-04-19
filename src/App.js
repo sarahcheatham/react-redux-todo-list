8c56bd7d81851cdd4ed9c9361b3a50ed694c911d
sarahcheatham <sarah.cheatham14@gmail.com>
2019-04-19 13:17:52 -0500

    add todo and showing todo list working, show active, completed etc not working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Footer from './components/Footer';
+import AddTodo from './containers/AddTodo';
+import VisibleTodoList from './containers/VisibleTodoList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        React Redux Todo List
-      </div>
-    );
-  }
-}
+const App = () => (
+    <div>
+        <AddTodo/>
+        <VisibleTodoList/>
+        <Footer/>
+    </div>
+)
 
 export default App;
+// import React, { Component } from 'react';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         React Redux Todo List
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
